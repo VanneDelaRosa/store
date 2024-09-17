0c1cc9abafd39ceacd11a3e656bbb7e0cd3545a7
Vanne Eloise Dela Rosa <vanneeloisedelarosa@Vannes-MacBook-Air.local>
2024-09-17 18:36:34 +0800

    Initialize React for Git

---
+++ src/components/FilterProductTable.jsx
@@ -0,0 +1,10 @@
+function FilterProductTable(props) {
+    const { children } = props;
+    return <>
+    <div>
+    {children}
+    </div>
+    </>;
+};
+
+export default FilterProductTable;
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar() {
+    return <div>
+        <input type="text" />
+        <div>
+            <input type="checkbox" />
+            Only shows product in stocs
+        </div>
+    </div>
+};
+export default SearchBar;
